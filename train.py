aed0c89a3d7094a1c91bb88beb26f1e083f438b6
saraehabb <s-sara.yehia@zewailcity.edu.eg>
2026-04-15 06:05:18 -0400

    [run-train] test success case

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 print("New change for pipeline test")
+print("Running training test with commit trigger")
 
 print("Starting training")
 
@@ -15,6 +16,3 @@ y_pred = model.predict(X)
 
 acc = accuracy_score(y, y_pred)
 print(f"Training done. Accuracy: {acc:.2f}")
-
-if acc < 0.95:
-    raise Exception("Model accuracy too low!")
