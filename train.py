4a472c30513481cc64183389118f8f021e4e3a71
saraehabb <s-sara.yehia@zewailcity.edu.eg>
2026-04-15 05:38:10 -0400

    initial commit

---
+++ train.py
@@ -0,0 +1,19 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+print("Starting training")
+
+X = np.random.rand(100, 3)
+y = (X.sum(axis=1) > 1.5).astype(int)
+
+model = LogisticRegression()
+model.fit(X, y)
+
+y_pred = model.predict(X)
+
+acc = accuracy_score(y, y_pred)
+print(f"Training done. Accuracy: {acc:.2f}")
+
+if acc < 0.95:
+    raise Exception("Model accuracy too low!")
